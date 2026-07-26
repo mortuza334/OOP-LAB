37cf6e6d295c58fc3852355e09fe379751ed889d
MD. Golam Mortuza <golammortuzapulok34@gmail.com>
2026-07-26 22:08:58 +0600

    WEEK4

--- problem1.java
+++ problem1.java
@@ -1,22 +1,28 @@
-import java.io.IOException;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        Locale.setDefault(Locale.US);
-        Scanner scanner = new Scanner(System.in);
-
-        String sellerName = scanner.next();
-        double fixedSalary = scanner.nextDouble();
-        double totalSales = scanner.nextDouble();
-
-        double totalSalary = fixedSalary + (totalSales * 0.15);
-
-        System.out.printf("TOTAL = R$ %.2f\n", totalSalary);
-
-        scanner.close();
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
     }
+}
 
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
 }
--- problem2.java
+++ problem2.java
@@ -1,36 +1,20 @@
-import java.io.IOException;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        Locale.setDefault(Locale.US);
-        Scanner scanner = new Scanner(System.in);
-
-        double salary = scanner.nextDouble();
-        int percentage;
-
-        if (salary >= 0 && salary <= 400.00) {
-            percentage = 15;
-        } else if (salary <= 800.00) {
-            percentage = 12;
-        } else if (salary <= 1200.00) {
-            percentage = 10;
-        } else if (salary <= 2000.00) {
-            percentage = 7;
-        } else {
-            percentage = 4;
-        }
-
-        double moneyEarned = salary * percentage / 100.0;
-        double newSalary = salary + moneyEarned;
-
-        System.out.printf("Novo salario: %.2f\n", newSalary);
-        System.out.printf("Reajuste ganho: %.2f\n", moneyEarned);
-        System.out.printf("Em percentual: %d %%\n", percentage);
-
-        scanner.close();
+class Arithmetic {
+    public int add(int a, int b) {
+        return a + b;
     }
+}
 
+class Adder extends Arithmetic {
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Adder a = new Adder();
+        
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());    
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
 }
